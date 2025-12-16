6d1542b11edc3bf4a2b72b0acf4d9e602c58d2f2
shadoprizm <142634171+shadoprizm@users.noreply.github.com>
2025-12-15 19:04:53 -0500

    Fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
     exclude: ['lucide-react'],
